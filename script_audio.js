c2327e8f3772165c6d349091f7f98bfcebadac72
ZET-Speech <hws00120@gmail.com>
2023-03-08 17:34:16 +0900

    UI / Sample form change

--- script_audio.js
+++ script_audio.js
@@ -1,4 +1,4 @@
-function saveSpeaker(speaker, emotion, dataset) {
+function comparison(speaker, emotion, dataset) {
     console.log(dataset.slice(0, -1))
     const table1 = document.querySelector(`#${dataset} #a`)
     let raw_data = document.createElement('td')
@@ -34,29 +34,86 @@ function saveSpeaker(speaker, emotion, dataset) {
 }
 
 
-const speakerArray_seen = [
+function allEmotion(speaker, emotion, dataset) {
+    console.log(dataset.slice(0, -1))
+    const table1 = document.querySelector(`#${dataset} #a`)
+    let raw_data = document.createElement('td')
+    raw_data.innerHTML = `<audio id="audio-player" controls="" preload="none" src="./wavs_${dataset.slice(0, -1)}/gss/${speaker}/${emotion}.wav" type="audio/mpeg">`
+    table1.appendChild(raw_data)
+    
+    const table2 = document.querySelector(`#${dataset} #b`)
+    let raw_data2 = document.createElement('td')
+    raw_data2.innerHTML = `<audio id="audio-player" controls="" preload="none" src="./wavs_${dataset.slice(0, -1)}/cg/${speaker}/${emotion}.wav" type="audio/mpeg">`
+    table2.appendChild(raw_data2)
+    
+    const table3 = document.querySelector(`#${dataset} #c`)
+    let raw_data3 = document.createElement('td')
+    raw_data3.innerHTML = `<audio id="audio-player" controls="" preload="none" src="./wavs_${dataset.slice(0, -1)}/cfg/${speaker}/${emotion}.wav" type="audio/mpeg">`    
+    table3.appendChild(raw_data3)
+    
+    // const table0 = document.querySelector(`#${dataset} #text`)
+    // let raw_data0 = document.createElement('td')
+    // fetch(`./wavs_${dataset.slice(0, -1)}/${speaker}/script.txt`)
+    // .then( response => response.text() )
+    // .then( text => raw_data0.innerText = text )
+    // table0.appendChild(raw_data0)
+
+    const table6 = document.querySelector(`#${dataset} #speaker`)
+    let raw_data6 = document.createElement('td')
+    raw_data6.innerText = decodeURIComponent(`${speaker}, ${emotion}`)
+    table6.appendChild(raw_data6)
+
+    const table7 = document.querySelector(`#${dataset} #ref`)
+    let raw_data7 = document.createElement('td')
+    raw_data7.innerHTML = `<audio id="audio-player" controls="" preload="none" src="./wavs_${dataset.slice(0, -1)}/cfg/${speaker}/ref.wav" type="audio/mpeg">`
+    table7.appendChild(raw_data7)
+}
+
+
+const comparisonSeenSpeaker = [
     "0015_OES", "0023_KSH"
 ]
-
-const speakerArray_unseen = [
+const comparisonUnseenSpeaker = [
     "N0262", "P0539"
 ]
-
-const emotionArray = [
+const comparisionEmotion = [
     "기쁨", "슬픔", "분노", "불안"
 ]
 
-const emotionEnglish = [
-    "happy", "sad", "angry", "anxious"
-]
-
-for (var i = 0; i < speakerArray_seen.length; i++) {
-    let seen_speaker = speakerArray_seen[i]
-    let unseen_speaker = speakerArray_unseen[i]
-    for (var j = 0; j < emotionArray.length; j++){
+for (var i = 0; i < comparisonSeenSpeaker.length; i++) {
+    let seen_speaker = comparisonSeenSpeaker[i]
+    let unseen_speaker = comparisonUnseenSpeaker[i]
+    for (var j = 0; j < comparisionEmotion.length; j++){
         let emotion = emotionArray[j]
-        saveSpeaker(seen_speaker, emotion, 'seen' + i.toString())
-        saveSpeaker(unseen_speaker, emotion, 'unseen' + i.toString())
-        //saveSpeaker(unseen_speaker, emotion, 'unseen_eng' + i.toString())
+        comparison(seen_speaker, emotion, 'seen' + i.toString())
+        comparison(unseen_speaker, emotion, 'unseen' + i.toString())
     }
 }
+
+const allEngUnseenSpeaker = [
+    "", "", "", ""
+]
+const allEngEmotion = [
+    "무감정", "기쁨", "슬픔", "분노", "불안"
+]
+
+const allSeenSpeaker = [
+    "0016_YSH", "0002_LYT", "0017_LSY", "0024_PGJ"
+]
+const allUnseenSpeaker = [
+    "S0071", "P0570", "S0098", "P0943"
+]
+const allKorEmotion = [
+    "무감정", "기쁨", "슬픔", "분노", "불안", "상처", "당황"
+]
+
+for (var i = 0; i < comparisonSeenSpeaker.length; i++) {
+    let seen_speaker = comparisonSeenSpeaker[i]
+    let unseen_speaker = comparisonUnseenSpeaker[i]
+    for (var j = 0; j < comparisionEmotion.length; j++){
+        let emotion = emotionArray[j]
+        comparison(seen_speaker, emotion, 'seen' + i.toString())
+        comparison(unseen_speaker, emotion, 'unseen' + i.toString())
+    }
+}
+
